52db616d6fe5f3968ef0669d79c579fca5d99167
woories19 <abdulllanasir@gmail.com>
2025-11-19 01:24:35 +0500

    v0.8.9.9

--- jobready/jobready.php
+++ jobready/jobready.php
@@ -5,7 +5,7 @@
  * Description: Your personal resume assistant. Upload, scan, and get instant feedback on how job-ready your resume really is.
  * Author: GitHub
  * Author URI: https://github.com/woories19
- * Version: 0.8.9.8
+ * Version: 0.8.9.9
  * Requires Plugins: elementor
  * Elementor tested up to: 3.32
  */
